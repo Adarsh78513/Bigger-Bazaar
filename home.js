0dcdf37037d6fb8c310dbf5b28f6e26a58ce7486
Adarsh78513 <adrsrj@gmail.com>
2022-04-22 15:10:34 +0530

    more pages added

--- home.js
+++ home.js
@@ -8,3 +8,31 @@ app.listen(8080);
 app.get('/', (req, res) => {
     res.render('home');
 });
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+
+app.get('/wishlist', (req, res) => {
+    res.render('wishlist');
+});
+
+app.get('/account', (req, res) => {
+    res.render('account');
+});
+
+
+
+app.get('/buy', (req, res) => {
+    res.render('buy');
+});
+
+app.get('/product', (req, res) => {
+    res.render('product');
+});
+
