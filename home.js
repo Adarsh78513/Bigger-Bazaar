1c2fa1a50234ac609d278238ffa7a42b7df776a4
Adarsh78513 <adrsrj@gmail.com>
2022-04-23 19:13:35 +0530

    made connection with azure

--- home.js
+++ home.js
@@ -4,23 +4,13 @@ var mysql = require('mysql');
 
 
 
-// con.connect(function(err) {
-//     if (err) throw err;
-//     console.log("Connected!");
-// });
-
-// con.connect(function(err) {
-//     if (err) throw err;
-//     console.log("Connected!");
-// });
-
-////makign a connection with azure
-// var con = mysql.createConnection({
-//     host: "project-database.mysql.database.azure.com",
-//     user: "_admin",
-//     password: "Project@123",
-//     database: "project"
-// });
+//makign a connection with azure
+var con = mysql.createConnection({
+    host: "project-database.mysql.database.azure.com",
+    user: "_admin",
+    password: "Project@123",
+    database: "project"
+});
 
 // con.connect(function(err) {
 //     if (err) throw err;
@@ -29,34 +19,67 @@ var mysql = require('mysql');
 
 
 ////change the password and the databasename according to your database
-//connecting with the localhost
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "1234",
-    database: "project"
-});
-
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
+// //connecting with the localhost
+// var con = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "1234",
+//     database: "project"
+// });
 
 // con.connect(function(err) {
 //     if (err) throw err;
-//     con.query("SELECT * FROM customers", function (err, result, fields) {
-//         if (err) throw err;
-//         console.log(result);
-//     });
+//     console.log("Connected!");
 // });
+
+con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM customers", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+    });
+});
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.listen(8080);
+// app.get('/', (req, res) => {
+//     res.render('home');
+// });
+
+
+
+// app.get('/', (req, res) => {
+//     con.query("SELECT * FROM products", function (err, result, fields) {
+//         if (err) throw err;
+//         console.log(result);
+//         // res.render('home', {title }
+//         res.render('home');
+// });
+
+// app.get('/', (req, res) => {
+//     let sql = "SELECT * FROM products";
+//     con.query(sql, function (err, result, fields) {
+//         if (err) throw err;
+//         console.log(result);
+//         res.render('home');
+//     });
+// });
+
 app.get('/', (req, res) => {
-    res.render('home');
+    let sql = "SELECT * FROM products";
+    con.query(sql, function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        res.render('home', {title : 'products', result});
+    });
 });
+
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/login', (req, res) => {
     res.render('login');
