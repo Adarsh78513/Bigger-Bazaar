ab9e92e0c022b1b08f89efda2024deef58dea267
Adarsh78513 <adrsrj@gmail.com>
2022-04-25 17:55:32 +0530

    made page for the products

--- home.js
+++ home.js
@@ -28,18 +28,18 @@ var con = mysql.createConnection({
 //     database: "project"
 // });
 
-// con.connect(function(err) {
-//     if (err) throw err;
-//     console.log("Connected!");
-// });
-
 con.connect(function(err) {
     if (err) throw err;
-    con.query("SELECT * FROM customers", function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-    });
+    console.log("Connected!");
 });
+
+// con.connect(function(err) {
+//     if (err) throw err;
+//     con.query("SELECT * FROM customers", function (err, result, fields) {
+//         if (err) throw err;
+//         console.log(result);
+//     });
+// });
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -72,8 +72,18 @@ app.get('/', (req, res) => {
     let sql = "SELECT * FROM products";
     con.query(sql, function (err, result, fields) {
         if (err) throw err;
-        console.log(result);
+        // console.log(result);
         res.render('home', {title : 'products', result});
+    });
+});
+
+app.get('/:id',(req, res) => {
+    const id = req.params.id;
+    let sql = "SELECT * FROM products WHERE ProductID = ?";
+    con.query(sql, [id], function (err, result, fields) {
+        if (err) throw err;
+        // console.log(result);
+        res.render('product', { product: result, title : 'product'});
     });
 });
 
@@ -81,7 +91,7 @@ app.get('/groceries', (req, res) => {
     let sql = "SELECT * FROM groceries, products WHERE groceries.ProductID = products.ProductID";
     con.query(sql, function (err, result, fields) {
         if (err) throw err;
-        console.log(result);
+        // console.log(result);
         res.render('groceries', {title : 'groceries', result});
     });
 });
@@ -90,7 +100,7 @@ app.get('/clothing', (req, res) => {
     let sql = "SELECT * FROM clothing, products WHERE clothing.ProductID = products.ProductID";
     con.query(sql, function (err, result, fields) {
         if (err) throw err;
-        console.log(result);
+        // console.log(result);
         res.render('clothing', {title : 'clothing', result});
     });
 });
@@ -99,7 +109,7 @@ app.get('/electronics', (req, res) => {
     let sql = "SELECT * FROM electronics, products WHERE electronics.ProductID = products.ProductID";
     con.query(sql, function (err, result, fields) {
         if (err) throw err;
-        console.log(result);
+        // console.log(result);
         res.render('electronics', {title : 'electronics', result});
     });
 });
@@ -108,9 +118,9 @@ app.get('/deals', (req, res) => {
     let sql = "SELECT * FROM deals, groceries WHERE groceries.ProductID = deals.ProductID; SELECT * FROM deals, clothing WHERE clothing.ProductID = deals.ProductID; SELECT * FROM deals, electronics WHERE electronics.ProductID = deals.ProductID;";
     con.query(sql, [1, 2, 3], function (err, result, fields) {
         if (err) throw err;
-        console.log(result[0]);
-        console.log(result[1]);
-        console.log(result[2]);
+        // console.log(result[0]);
+        // console.log(result[1]);
+        // console.log(result[2]);
         res.render('deals', {title : 'deals', result});
     });
 });
@@ -119,9 +129,9 @@ app.get('/product', (req, res) => {
     let sql = "SELECT * FROM deals, groceries WHERE groceries.ProductID = deals.ProductID; SELECT * FROM deals, clothing WHERE clothing.ProductID = deals.ProductID; SELECT * FROM deals, electronics WHERE electronics.ProductID = deals.ProductID;";
     con.query(sql, [1, 2, 3], function (err, result, fields) {
         if (err) throw err;
-        console.log(result[0]);
-        console.log(result[1]);
-        console.log(result[2]);
+        // console.log(result[0]);
+        // console.log(result[1]);
+        // console.log(result[2]);
         res.render('product', {title : 'product', result});
     });
 });
